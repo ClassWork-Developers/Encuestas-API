38c33bbe0fbef4508564ac568d88963b2cd10649
jessra <avilab.sara@gmail.com>
2023-04-02 12:03:50 -0400

    modificadas funciones de crear pregunta y respuesta

--- Server/src/controllers/Admin/CrearPregunta.js
+++ Server/src/controllers/Admin/CrearPregunta.js
@@ -2,41 +2,24 @@ import { PreguntasModel } from '../../Schemas/Preguntas.js';
 import { RelacionModel } from '../../Schemas/Relacion.js';
 
 export async function CrearPregunta(req, res) {
-  // const { pregunta, id_encuesta } = req.body;
   const { array_preguntas, id_encuesta } = req.body;
 
-  var target = array_preguntas.length - 1;
-
-  array_preguntas.forEach((element, index) => {
-    let Pregunta = new PreguntasModel({
-      pregunta: element.pregunta,
-    });
-
-    if (index == target) {
+  try {
+    array_preguntas.forEach((element, index) => {
+      let Pregunta = new PreguntasModel({
+        pregunta: element.pregunta,
+      });
       Pregunta.save()
         .then((response) => {
-          res.status(201).json({ data: { status: true, id_pregunta: response._id, id_encuesta: response.id_encuesta }, r: true });
           let Relacion = new RelacionModel({
             id_pregunta: response._id,
             id_encuesta: id_encuesta,
           });
-          Relacion.save().then((res) => {
-            let obj = { status: true, id_: res._id };
-            console.log(obj);
-          });
+          Relacion.save()
         })
-        .catch((err) => res.status(404).json({ msg: 'Ha ocurrido un error creando la pregunta, intentelo de nuevo. ' + err, r: false }));
-    } else {
-      Pregunta.save().then((response) => {
-        let Relacion = new RelacionModel({
-          id_pregunta: response._id,
-          id_encuesta: id_encuesta,
-        });
-        Relacion.save().then((res) => {
-          let obj = { status: true, id_: res._id };
-          console.log(obj);
-        });
-      });
-    }
-  });
+    })
+    res.status(201).json({ r: true });
+    } catch (err) {
+    res.status(404).json({ msg: 'Ha ocurrido un error creando la pregunta, intentelo de nuevo. ' + err, r: false });
+  }
 }
--- Server/src/controllers/Client/CrearRespuesta.js
+++ Server/src/controllers/Client/CrearRespuesta.js
@@ -1,18 +1,20 @@
 import { RespuestasModel } from '../../Schemas/Respuestas.js';
 
 export async function CrearRespuesta(req, res) {
-  const { id_encuesta, id_pregunta, id_usuario, nombre, icon, respuesta } = req.body;
+  const { id_encuesta, id_usuario, respuestas } = req.body;
 
-  let Respuesta = new RespuestasModel({
-    id_encuesta: id_encuesta,
-    id_pregunta: id_pregunta,
-    id_usuario: id_usuario,
-    nombre: nombre,
-    icon: icon,
-    respuesta: respuesta,
-  });
-
-  Respuesta.save()
-  .then(() => res.status(201).json({r: true}))
-  .catch((err) => res.status(404).json({msg: 'Ha ocurrido un error registrando la respuesta, intentelo de nuevo. ' + err, r: false}))
+  try {
+    respuestas.forEach((element, index) => {
+      let Respuesta = new RespuestasModel({
+        id_encuesta: id_encuesta,
+        id_pregunta: element.id,
+        id_usuario: id_usuario,
+        respuesta: element.respuesta,
+      });
+      Respuesta.save()
+    })
+    res.status(201).json({r: true})
+  } catch (err) {
+    res.status(404).json({msg: 'Ha ocurrido un error registrando la respuesta, intentelo de nuevo. ' + err, r: false})
+  }
 }
